037ef0f7079b3949d3bdd7e68145e250ccd62903
Arun Kanagolkar <138815437+ArunKanagolkar@users.noreply.github.com>
2025-01-18 23:09:25 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,10 +1,10 @@
 
 
 import streamlit as st
-import mymodel as m
+
 
 st.write("Hello world")
 st.write('This My first App Deploy')
 w = st.slider('Forecast with Days')
 f = w*2
-st.write(m.run(windows=f))
+st.write(windows=f))
