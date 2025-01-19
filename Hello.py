9fbe971f72f0d2eae2e15695ae528c5ee9a8b4e3
Arun Kanagolkar <138815437+ArunKanagolkar@users.noreply.github.com>
2025-01-19 22:23:00 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 st.title(" POWER CALCULATION")
 st.write('3ph AC power calculator')
 v = st.slider('Select voltage')
-i = v = st.slider('Select current')
+i = st.slider('Select current')
 p = 1.73*v*i*0.85
 st.write('3ph AC power')
 st.text(p)
