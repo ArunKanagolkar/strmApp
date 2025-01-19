075e139e30240e14359d96efa2a0ffd6f4ec26b2
Arun Kanagolkar <138815437+ArunKanagolkar@users.noreply.github.com>
2025-01-19 22:58:29 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -5,10 +5,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-st.title(" POWER CALCULATION")
+st.title(" ELE.POWER CALCULATION")
 st.write('3ph AC power calculator')
 v = st.slider('Select voltage')
 i = st.slider('Select current')
-p = 1.73*v*i*0.85
-st.write('3ph AC power')
-st.text(p)
+option = st.selectbox(
+    "Select type of power",
+    ("Three phase", "Single phase"),
+)
+
+st.write("You selected:", option)
+if option == 'Three phase':
+  p = (1.73*v*i*0.85)/1000
+  st.write('3ph AC power in KW')
+  st.text(p)
+elif:
+  p = (v*i*0.85)/1000
+  st.write('1ph AC power in KW')
+  st.text(p)
+  
